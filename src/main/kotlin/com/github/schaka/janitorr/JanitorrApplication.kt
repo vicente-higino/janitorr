3a53dfa5d887ed5dda56c1acb41c7c849b5656f1
Vicente Higino <vi_higino@hotmail.com.br>
2026-08-14 04:17:52 -0300

    Register API models for native binding

--- src/main/kotlin/com/github/schaka/janitorr/JanitorrApplication.kt
+++ src/main/kotlin/com/github/schaka/janitorr/JanitorrApplication.kt
@@ -18,7 +18,42 @@ import java.nio.file.Path
 @EnableScheduling
 @ConfigurationPropertiesScan
 @SpringBootApplication
-@RegisterReflectionForBinding(Logger.Level::class)
+@RegisterReflectionForBinding(
+    classes = [Logger.Level::class],
+    classNames = [
+        "com.github.schaka.janitorr.mediaserver.api.MediaServerUser",
+        "com.github.schaka.janitorr.mediaserver.library.VirtualFolderResponse",
+        "com.github.schaka.janitorr.mediaserver.library.AddLibraryRequest",
+        "com.github.schaka.janitorr.mediaserver.library.CollectionResponse",
+        "com.github.schaka.janitorr.mediaserver.library.AddPathRequest",
+        "com.github.schaka.janitorr.mediaserver.library.LibraryContent",
+        "com.github.schaka.janitorr.mediaserver.library.items.ItemPage",
+        "com.github.schaka.janitorr.mediaserver.library.items.MediaFolderItem",
+        "com.github.schaka.janitorr.mediaserver.emby.library.AddVirtualFolder",
+        "com.github.schaka.janitorr.mediaserver.emby.library.AddMediaPathRequest",
+        "com.github.schaka.janitorr.seerr.paging.SeerrPage",
+        "com.github.schaka.janitorr.seerr.requests.RequestResponse",
+        "com.github.schaka.janitorr.seerr.servarr.ServarrSettings",
+        "com.github.schaka.janitorr.servarr.bazarr.BazarrPage",
+        "com.github.schaka.janitorr.servarr.history.HistoryResponse",
+        "com.github.schaka.janitorr.servarr.history.SonarrHistoryResponse",
+        "com.github.schaka.janitorr.servarr.radarr.movie.MoviePayload",
+        "com.github.schaka.janitorr.servarr.radarr.movie.MovieFile",
+        "com.github.schaka.janitorr.servarr.data_structures.Tag",
+        "com.github.schaka.janitorr.servarr.quality_profile.QualityProfile",
+        "com.github.schaka.janitorr.servarr.data_structures.RadarrImportListExclusion",
+        "com.github.schaka.janitorr.servarr.sonarr.series.SeriesPayload",
+        "com.github.schaka.janitorr.servarr.sonarr.episodes.EpisodeResponse",
+        "com.github.schaka.janitorr.servarr.sonarr.episodes.MonitoringRequest",
+        "com.github.schaka.janitorr.servarr.data_structures.SonarrImportListExclusion",
+        "com.github.schaka.janitorr.stats.janitorrstats.requests.JanitorrStatsPagedResponse",
+        "com.github.schaka.janitorr.stats.janitorrstats.requests.JanitorrStatsPlayEvent",
+        "com.github.schaka.janitorr.stats.jellystat.requests.JellystatPage",
+        "com.github.schaka.janitorr.stats.jellystat.requests.JellyStatHistoryResponse",
+        "com.github.schaka.janitorr.stats.jellystat.requests.JellystatItemRequest",
+        "com.github.schaka.janitorr.stats.streamystats.requests.StreamystatsHistoryResponse",
+    ]
+)
 class JanitorrApplication {
 
 }
